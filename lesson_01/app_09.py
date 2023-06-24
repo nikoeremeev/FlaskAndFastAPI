6de1d6052ff86077858fe2aa82e27f7adb3601b5
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-06-24 11:21:11 +0300

    add tasks for lesson_01

---
+++ lesson_01/app_09.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hi!'
+
+
+@app.route('/poems/')
+def poems():
+    context = {'poem': ['Вот не думал, не гадал,',
+                        'Программистом взял и стал.',
+                        'Хитрый знает он язык,',
+                        'Он к другому не привык.',
+                        ]}
+    # txt = """<h1>Стихотворение</h1>\n<p>""" + '<br/>'.join(poem) + '</p>'
+    return render_template('poems.html', **context)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
