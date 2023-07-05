90eaf9cf42717e738d7526488ee75b79fa2d1db6
Nikolay Eremeev <nikoeremeev@gmail.com>
2023-07-05 21:28:53 +0300

    add some tasks for lesson 3

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-# from seminar_02.tasks import app
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
+from lesson_03.app_07 import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
